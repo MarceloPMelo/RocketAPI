599421b93b094c39a9054cb3c1b69c6dd54f14c9
Marcelo Melo <mpedrosa.melo@gmail.com>
2025-05-31 20:46:57 -0300

    removeCart

--- product-api/src/cart/cart.controller.ts
+++ product-api/src/cart/cart.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Request, Body } from '@nestjs/common';
+import { Controller, Get, Post, Request, Body, Param, ParseIntPipe, Delete } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { Auth } from 'src/auth/auth.decorator';
+import { CartDto } from './dto/cart.dto';
 
 @Controller('cart')
 export class CartController {
@@ -12,9 +13,15 @@ export class CartController {
         return this.cartService.getCart(req.user.id);
     }
 
-    @Post('add')
+    @Post('add/:productId')
     @Auth()
-    async addToCart(@Request() req: any, @Body() body: { productId: number}) {
-        return this.cartService.addToCart(body.productId, req.user.id);
+    async addToCart(@Request() req: any, @Param('productId', ParseIntPipe) productId: number) {
+        return this.cartService.addToCart(productId, req.user.id);
+    }
+
+    @Delete('remove/:productId')
+    @Auth()
+    async removeFromCart(@Request() req: any, @Param('productId', ParseIntPipe) productId: number) {
+        return this.cartService.removeFromCart(productId, req.user.id);
     }
 }
--- product-api/src/cart/cart.service.ts
+++ product-api/src/cart/cart.service.ts
@@ -22,36 +22,136 @@ export class CartService {
         });
     }
 
+    
+
+    
+
     async addToCart(productId: number, userId: number) {
+
+        //verificar se o productId é um número e positivo
+        if (isNaN(productId) || productId <= 0) {
+            return {
+                message: 'Product ID must be a positive number',
+                statusCode: 400
+            }
+        }
+
         const cart = await this.prisma.cart.findFirst({
             where: { userId }
         });
 
         if (!cart) {
-            throw new Error('Cart not found');
+            return {
+                message: 'Cart not found',
+                statusCode: 404
+            }
         }
+
+        const product = await this.prisma.product.findUnique({
+            where: { id: productId }
+        });
+
+        if (!product) {
+            return {
+                message: 'Product not found',
+                statusCode: 404
+            }
+        }
+
         const cartItem = await this.prisma.cartItem.findFirst({
             where: { productId, cartId: cart.id }
         });
 
         if (cartItem) {
-            return this.prisma.cartItem.update({
+            const updatedItem = await this.prisma.cartItem.update({
                 where: { id: cartItem.id },
                 data: {
                     quantity: cartItem.quantity + 1
                 }
             });
+            return {
+                message: 'Item quantity updated successfully',
+                statusCode: 200,
+                data: updatedItem
+            };
         }
 
         else {
-            return this.prisma.cartItem.create({
+            const newItem = await this.prisma.cartItem.create({
                 data: {
                     productId,
                     cartId: cart.id,
                     quantity: 1,
-                    unitPrice: 0
+                    unitPrice: product.price
                 }
             });
+            return {
+                message: 'Item added to cart successfully',
+                statusCode: 201,
+                data: newItem
+            };
         }
     }
+
+    async removeFromCart(productId: number, userId: number) {
+        //verificar se o productId é um número e positivo
+        if (isNaN(productId) || productId <= 0) {
+            return {
+                message: 'Product ID must be a positive number',
+                statusCode: 400
+            }
+        }
+        
+        const cart = await this.prisma.cart.findFirst({
+            where: { userId }
+        });
+
+        if (!cart) {
+            return {
+                message: 'Cart not found',
+                statusCode: 404
+            }
+        }
+
+        const cartItem = await this.prisma.cartItem.findFirst({
+            where: { productId, cartId: cart.id }
+        });
+
+        if (!cartItem) {
+            return {
+                message: 'Item not found in cart',
+                statusCode: 404
+            }
+        }
+
+        if (cartItem.quantity > 1) {
+            const updatedItem = await this.prisma.cartItem.update({
+                where: { id: cartItem.id },
+                data: {
+                    quantity: cartItem.quantity - 1
+                }
+            });
+            return {
+                message: 'Item quantity updated successfully',
+                statusCode: 200,
+                data: updatedItem
+            };
+        }
+
+        else {
+            const deletedItem = await this.prisma.cartItem.delete({
+                where: { id: cartItem.id }
+            });
+            return {
+                message: 'Item removed from cart successfully',
+                statusCode: 200,
+                data: deletedItem
+            };
+        }
+
+        
+        
+        
+    }
+
 }
