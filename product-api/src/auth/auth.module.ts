6ce4146410ce350400423e33778a4411c17c64a9
Marcelo Melo <mpedrosa.melo@gmail.com>
2025-06-02 19:37:02 -0300

    Read.Me e .env

--- product-api/src/auth/auth.module.ts
+++ product-api/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { CartService } from 'src/cart/cart.service';
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'c85651ceac34d0d0ebe076d1b34af6f12b7aed0f482642ae2bedebf75b1765e0a5b07866173b2b69adb5c48bbbd7bb0f5271adf0f7f349416cd5e8fe73e148cf', // Em produção, use variáveis de ambiente
+      secret: process.env.JWT_SECRET, // Em produção, use variáveis de ambiente
       signOptions: { expiresIn: '1h' },
     }),
   ],
